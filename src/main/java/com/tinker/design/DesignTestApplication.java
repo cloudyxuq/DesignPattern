b677bb9e335c7bd591c2302faf4e9b8fbf415ec5
xuqiang <275467298@qq.com>
2019-10-30 15:50:04 +0800

    Initial commit

---
+++ src/main/java/com/tinker/design/DesignTestApplication.java
@@ -0,0 +1,13 @@
+package com.tinker.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignTestApplication.class, args);
+    }
+
+}
